a81ffd2e3d963a8b0843fc5572e122f828a9b083
Ravi <ravirana75036@gmail.com>
2025-03-01 14:46:32 +0530

    checl

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const mongoURI = process.env.MONGO_URI || "";           
+const mongoURI = process.env.MONGO_URI || '';           
 
 export const connectDB = async () => {
     try {
